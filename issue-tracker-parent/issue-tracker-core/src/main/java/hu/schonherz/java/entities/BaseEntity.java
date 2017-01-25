2d8ded3bcf7ca8769e9c44ddddb15d6a0e9db1f8
Kocsi Mónika <monika.kocsi@neuron.hu>
2017-01-25 22:34:13 +0100

    BaseEntity update

--- issue-tracker-parent/issue-tracker-core/src/main/java/hu/schonherz/java/entities/BaseEntity.java
+++ issue-tracker-parent/issue-tracker-core/src/main/java/hu/schonherz/java/entities/BaseEntity.java
@@ -1,9 +1,15 @@
 package hu.schonherz.java.entities;
 
+import java.util.Date;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @MappedSuperclass
 public class BaseEntity {
@@ -11,13 +17,64 @@ public class BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	
+	String recUser;
+	String modUser;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	Date recDate;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	Date modDate;
 
+	@PrePersist
+	public void prePersist() {
+		recDate = new Date();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		modDate = new Date();
+	}
+	
 	public Long getId() {
 		return id;
 	}
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getRecUser() {
+		return recUser;
+	}
+
+	public void setRecUser(String recUser) {
+		this.recUser = recUser;
+	}
+
+	public String getModUser() {
+		return modUser;
+	}
+
+	public void setModUser(String modUser) {
+		this.modUser = modUser;
+	}
+
+	public Date getRecDate() {
+		return recDate;
+	}
+
+	public void setRecDate(Date recDate) {
+		this.recDate = recDate;
+	}
+
+	public Date getModDate() {
+		return modDate;
+	}
+
+	public void setModDate(Date modDate) {
+		this.modDate = modDate;
 	}
 	
 	
