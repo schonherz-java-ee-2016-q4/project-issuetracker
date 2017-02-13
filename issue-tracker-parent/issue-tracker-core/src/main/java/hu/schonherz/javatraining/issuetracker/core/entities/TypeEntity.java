ea94706325479a2d3f49dac452cc3ebe634903c9
DavidKiraly <hundood@gmail.com>
2017-02-13 15:46:16 +0100

    type service test now works, little bug fix in hu.properties

--- issue-tracker-parent/issue-tracker-core/src/main/java/hu/schonherz/javatraining/issuetracker/core/entities/TypeEntity.java
+++ issue-tracker-parent/issue-tracker-core/src/main/java/hu/schonherz/javatraining/issuetracker/core/entities/TypeEntity.java
@@ -2,6 +2,7 @@ package hu.schonherz.javatraining.issuetracker.core.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -24,8 +25,8 @@ public class TypeEntity extends BaseEntity implements Serializable {
 
 	private String name;
 	private String description;
-	
-	@ManyToOne
+
+	@ManyToOne(cascade = CascadeType.ALL)
 	private CompanyEntity company;
 	
 	@ManyToOne
