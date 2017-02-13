bbb3f99f63475071cd566630aa06573a8e5aeb55
BecseiMiklos <bmikyy@gmail.com>
2017-02-13 16:03:07 +0100

    TicketService test added, minor fixes

--- issue-tracker-parent/issue-tracker-core/src/main/java/hu/schonherz/javatraining/issuetracker/core/entities/TicketEntity.java
+++ issue-tracker-parent/issue-tracker-core/src/main/java/hu/schonherz/javatraining/issuetracker/core/entities/TicketEntity.java
@@ -1,18 +1,10 @@
 package hu.schonherz.javatraining.issuetracker.core.entities;
 
+import lombok.*;
+
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
-
-import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "ticket")
@@ -22,25 +14,25 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class TicketEntity extends BaseEntity implements Serializable {
-	private static final long serialVersionUID = -3385748756482915829L;
+    private static final long serialVersionUID = -3385748756482915829L;
 
-	private String uid;
-	private String title;
-	private String description;
-	private String clientMail;
-	
-	@ManyToOne
-	private TypeEntity type;
-	
-	@ManyToOne
-	private StatusEntity currentStatus;
-	
-	@ManyToOne
-	private UserEntity user;
-	
-	@ManyToMany
-	private List<CommentEntity> comments;
-	
-	@ManyToMany
-	private List<HistoryEntity> history;
+    private String uid;
+    private String title;
+    private String description;
+    private String clientMail;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private TypeEntity type;
+
+    @ManyToOne
+    private StatusEntity currentStatus;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private UserEntity user;
+
+    @ManyToMany
+    private List<CommentEntity> comments;
+
+    @ManyToMany
+    private List<HistoryEntity> history;
 }
